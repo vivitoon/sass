40b71b1606e392f2d15f47ac4a8802c563c954e9
Chris Eppstein <chris@eppsteins.net>
2013-06-06 11:46:27 -0700

    Value helper for creating a color from hsl components.
    Change-Id: I7e0c4b80c52f7e336f9def8f9e74b6e5a14d9b88

--- lib/sass/script/value/helpers.rb
+++ lib/sass/script/value/helpers.rb
@@ -23,5 +23,23 @@ module Sass::Script::Value
     def hex_color(value, alpha = nil)
       Color.from_hex(value, alpha)
     end
+
+    # Construct a Sass Color from hsl values.
+    #
+    # @since `3.3.0`
+    # @param hue [::Number] The hue of the color in degrees. A number generally
+    #                       between 0 and 360, but it must only be non-negative.
+    # @param saturation [::Number] The saturation of the color.
+    #                              Must be between 0 and 100 inclusive.
+    # @param lightness [::Number] The lightness of the color.
+    #                             Must be between 0 and 100 inclusive.
+    # @param alpha [::Number] The alpha channel. A number between 0 and 1.
+    #
+    # @return [Sass::Script::Value::Color] the color object
+    def hsl_color(hue, saturation, lightness, alpha = nil)
+      attrs = {:hue => hue, :saturation => saturation, :lightness => lightness}
+      attrs[:alpha] = alpha if alpha
+      Color.new(attrs)
+    end
   end
 end
